07d5c772e2a5bd0296cbbd480b88432032a5e338
sahitya4ud <sahitya4ud@gmail.com>
2024-09-19 10:58:42 -0500

    employee1 information

---
+++ src/main/java/org/example/Employer1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Employer1 {
+    public String name;
+    public int age;
+    public String designation;
+    public int employeeId;
+    public String address;
+
+}
